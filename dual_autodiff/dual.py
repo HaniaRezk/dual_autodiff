8fbecea5b0bcbade39d0061aa5d4dda8ae32dad2
Hania Rezk <118572565+HaniaRezk@users.noreply.github.com>
2024-11-24 17:20:24 +0000

    added extra functions

--- dual_autodiff/dual.py
+++ dual_autodiff/dual.py
@@ -1,18 +1,20 @@
 import numpy as np
-#import logging
+import logging
 
-#logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
 
 class Dual:
     def __init__(self,real,dual):
         self.real=real
         self.dual=dual
 
+    
+    #Redefining the __str__ method to get the desired print format
     def __str__(self):
         string="Dual(real="+ str(self.real) +", dual="+ str(self.dual)+ ")."
         return string
 
-
+    #Redefining the basic arithmetic operations
     def __add__(self,x):
         return Dual(x.real+self.real,x.dual+self.dual)
     
@@ -21,17 +23,17 @@ class Dual:
         self.dual+=x.dual
         return self
 
-    def __div__(self,x):
+    def __truediv__(self,x):
         if (x.real==0):
-            #logging.warning("Division of dual numbers is not defined when the real part of the denominator is zero")
+            logging.warning("Division of dual numbers is not defined when the real part of the denominator is zero")
             return np.nan
         real=self.real/x.real
         dual=(self.dual*x.real-self.real*x.dual)/(x.real**2)
         return Dual(real,dual)
 
-    def __idiv__(self,x):
+    def __itruediv__(self,x):
         if (x.real==0):
-            #logging.warning("Division of dual numbers is not defined when the real part of the denominator is zero")
+            logging.warning("Division of dual numbers is not defined when the real part of the denominator is zero")
             return np.nan
         self.real/=x.real
         dual=(self.dual*x.real-self.real*x.dual)/(x.real**2)
@@ -57,7 +59,29 @@ class Dual:
         self.real=real
         self.dual=dual
         return self
+    
+    def __pow__(self,power):
+        real=self.real**power
+        dual=(power*self.real**(power-1))*self.dual
+        return Dual(real,dual)
+    
+    def __ipow__(self,power):
+        real=self.real**power
+        dual=(power*self.real**(power-1))*self.dual
+        self.real=real
+        self.dual=dual
+        return self
 
+
+    def __neg__(self):
+        return Dual(-self.real,-self.dual)
+
+    def __eq__(self,x):
+        return ((self.real==x.real) and (self.dual==x.dual))
+
+    def __abs__(self):
+        return Dual(np.abs(self.real),np.abs(self.dual))
+    #Defining other essential functions (details in the project report)
     def sin(self):
         dual=self.dual*np.cos(self.real)
         return  Dual(np.sin(self.real),dual)
@@ -68,14 +92,14 @@ class Dual:
 
     def tan(self):
         if (np.cos(self.real)==0):
-            #logging.warning("Tan can't be defined for this function")
+            logging.warning("Tan can't be defined for this function")
             return np.nan
         dual=self.dual/(np.cos(self.real)**2)
         return  Dual(np.tan(self.real),dual)
 
     def log(self):
         if (self.real==0):
-            #logging.warning("Logarithm of dual number not defined when real part is zero")
+            logging.warning("Logarithm of dual number not defined when real part is zero")
             return np.nan
         dual=(1/self.real)*self.dual
         return  Dual(np.log(self.real),dual)
@@ -97,6 +121,15 @@ class Dual:
 
     def ceil(self):
         return  Dual(np.ceil(self.real),np.ceil(self.dual))
+
+    def inverse(self):
+        if (self.real==0):
+            logging.warning("Can't invert this Dual number")
+            return np.nan
+        real=1/self.real
+        dual=-self.dual/(self.real**2)
+
+        return  Dual(real,dual)
 
 
     
